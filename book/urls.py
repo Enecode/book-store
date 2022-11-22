1c3b6152c3d3138d8f1378704e307a6238ef764f
Isah Jacob <isahjacob0@gmail.com>
2022-11-22 23:34:04 +0100

    Added publisher detail path

--- book/urls.py
+++ book/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "book"
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path("<int:pk>/", views.publisher_details, name="publisher_details")
 ]
