132251aec8eb092ea2602718f7cf9240fe520ebc
Isah Jacob <isahjacob0@gmail.com>
2022-11-24 13:00:08 +0100

    import decorators and response from rest framework and display 'Hello API'

--- book/views.py
+++ book/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Publisher
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-# Create your views here.
-def index(request):
-    queryset = Publisher.objects.all()
-    return render(request, 'book/index.html', context={"publishers": list(queryset)})
-
-
-def publisher_details(request, pk):
-    publisher = get_object_or_404(Publisher, pk=pk)
-    return render(request, "book/publisher_detail.html", context={"publisher": publisher})
+@api_view()
+def book_list(request):
+    return Response("Hello API")
