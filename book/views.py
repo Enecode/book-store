79d5ac879b674860341609be3f4b7ea3350eca40
Isah Jacob <isahjacob0@gmail.com>
2022-11-24 15:02:05 +0100

    se

--- book/views.py
+++ book/views.py
@@ -1,7 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Book
+from .serialization import BookSerialization
 
-@api_view()
+
+@api_view(['GET', 'GET'])
 def book_list(request):
+    queryset = Book.objects.all()
+    serializer = BookSerialization(queryset, many=True)
     return Response("Hello API")
