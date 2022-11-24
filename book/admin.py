2deca9348a02740508167bc563f3246ac170ef8f
Isah Jacob <isahjacob0@gmail.com>
2022-11-24 12:37:10 +0100

    Created an admin that takes over from django admin

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Publisher, Book
+
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'isbn', 'publisher']
+    list_editable = ['isbn']
+    list_filter = ['publisher', 'date_published']
+    search_fields = ['title_istartswith', 'isbn']
+    date_hierarchy = 'date_published'
+
+
+admin.site.register(Book)
+admin.site.register(Publisher)
