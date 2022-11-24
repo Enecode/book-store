377d91454d66b7c8e226fcaff57573dbe2504b91
Isah Jacob <isahjacob0@gmail.com>
2022-11-24 12:56:37 +0100

    added the url path for the rest framework path comment out the previous url paths

--- book/urls.py
+++ book/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 
 from . import views
 
-app_name = "book"
+# app_name = "book"
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path("<int:pk>/", views.publisher_details, name="publisher_details"),
+#     path('book_list/', views.book_list, name="book_list"),
+#     path('book_create/', views.book_create, name='book_create')
+# ]
 urlpatterns = [
-    path('', views.index, name='index'),
-    path("<int:pk>/", views.publisher_details, name="publisher_details")
+    path("book/", views.book_list, name="book_list")
 ]
